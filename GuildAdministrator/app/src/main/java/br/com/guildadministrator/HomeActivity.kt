148f1e30b8f8fe294e6e472177d4447ad25f830b
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2020-09-21 10:33:19 -0300

    feat: criação da tela de home da aplicação e redirecionamento ao clicar em login
    Foi implementado o redirecionamento para a tela de Home quando se clica
    no botão de login, esse Home no momento é uma tela não implementada.

---
+++ GuildAdministrator/app/src/main/java/br/com/guildadministrator/HomeActivity.kt
@@ -0,0 +1,13 @@
+package br.com.guildadministrator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        title = "Home"
+    }
+}
--- GuildAdministrator/app/src/main/java/br/com/guildadministrator/MainActivity.kt
+++ GuildAdministrator/app/src/main/java/br/com/guildadministrator/MainActivity.kt
@@ -1,7 +1,9 @@
 package br.com.guildadministrator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         title = "Adiministrador de Guilds Albion Online"
+
+        button_login_main_activity.setOnClickListener { redirectToHome() }
     }
+
+    private fun redirectToHome() = startActivity(Intent(this, HomeActivity::class.java))
 }
