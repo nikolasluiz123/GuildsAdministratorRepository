36e073b97455e147bd61a914eb59570e8a2b7fa2
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2020-09-24 07:55:53 -0300

    feat: adicionado texto no fragment de notificações pra saber que é ele o selecionado
    Só foi adicionado um text view pra ter um texto e sabermos qual é o
    fragment selecionado no menu.

--- GuildAdministrator/app/src/main/java/br/com/guildadministrator/NotificacoesFragment.kt
+++ GuildAdministrator/app/src/main/java/br/com/guildadministrator/NotificacoesFragment.kt
@@ -8,6 +8,11 @@ import androidx.fragment.app.Fragment
 
 class NotificacoesFragment : Fragment() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_notificacoes, container, false)
     }
